b8dd5da25ac233fa1546a4ed31e69e4f60313708
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-08-14 16:23:22 +0600

    push

--- app/src/main/java/com/example/carcatalogue/MainActivity.kt
+++ app/src/main/java/com/example/carcatalogue/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.carcatalogue
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DrawerValue
@@ -18,11 +20,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.carcatalogue.ui.theme.CarCatalogueTheme
 import com.example.carcatalogue.ui_components.DrawerMenu
 import com.example.carcatalogue.ui_components.MainListItem
 import com.example.carcatalogue.ui_components.MainTopBar
 import com.example.carcatalogue.utils.DrawerEvents
+import com.example.carcatalogue.utils.ListItem
+import com.example.carcatalogue.utils.idArrayList
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +39,7 @@ class MainActivity : ComponentActivity() {
             val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
             val coroutineScope = rememberCoroutineScope()
             val mainList = remember {
-                mutableStateOf()
+                mutableStateOf(getListItemsByIndex(0, this))
             }
             val topBarTitle = remember {
                 mutableStateOf("Porsche")
@@ -47,6 +52,7 @@ class MainActivity : ComponentActivity() {
                             when(event) {
                                 is DrawerEvents.OnItemClick -> {
                                     topBarTitle.value = event.title
+                                    mainList.value = getListItemsByIndex(event.index, this@MainActivity)
                                 }
                             }
                             coroutineScope.launch {
@@ -59,13 +65,14 @@ class MainActivity : ComponentActivity() {
                         topBar = {
                             MainTopBar(title = topBarTitle.value, drawerState)
                         },
-
                     ) {
                         LazyColumn(
-                            modifier = Modifier.fillMaxSize()
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(top = 55.dp)
                         ) {
-                            items() {
-                                MainListItem(item = )
+                            items(mainList.value) {
+                                MainListItem(item = it)
                             }
                         }
                     }
@@ -73,5 +80,17 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+private fun getListItemsByIndex(index: Int, context: Context): List<ListItem> {
+    val list = ArrayList<ListItem>()
+    val arraylist = context.resources.getStringArray(idArrayList.listId[index])
+    arraylist.forEach { item ->
+        val itemArr = item.split("|")
+        list.add(
+            ListItem(itemArr[0], itemArr[1])
+        )
+    }
+    return list
 }
 
---
+++ app/src/main/java/com/example/carcatalogue/utils/idArrayList.kt
@@ -0,0 +1,14 @@
+package com.example.carcatalogue.utils
+
+import com.example.carcatalogue.R
+
+object idArrayList  {
+    val listId = listOf(
+        R.array.porsche,
+        R.array.bmw,
+        R.array.lamborghini,
+        R.array.mercedes,
+        R.array.rr,
+        R.array.bentley
+    )
+}
